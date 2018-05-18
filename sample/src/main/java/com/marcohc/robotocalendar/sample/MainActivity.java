eba987c2ec9db9890bafec7f2062978b5b24a583
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2018-05-18 18:33:49 +0200

    Added clear selected day method. Added annotations. Date instead of calendar parameters

--- sample/src/main/java/com/marcohc/robotocalendar/sample/MainActivity.java
+++ sample/src/main/java/com/marcohc/robotocalendar/sample/MainActivity.java
@@ -18,7 +18,6 @@ package com.marcohc.robotocalendar.sample;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -51,27 +50,28 @@ public class MainActivity extends AppCompatActivity implements RobotoCalendarLis
         // Gets the calendar from the view
         robotoCalendarView = findViewById(R.id.robotoCalendarPicker);
         Button markDayButton = findViewById(R.id.markDayButton);
-        markDayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar calendar = Calendar.getInstance();
-                Random random = new Random(System.currentTimeMillis());
-                int style = random.nextInt(2);
-                int daySelected = random.nextInt(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
-                calendar.set(Calendar.DAY_OF_MONTH, daySelected);
+        Button clearSelectedDayButton = findViewById(R.id.clearSelectedDayButton);
 
-                switch (style) {
-                    case 0:
-                        robotoCalendarView.markCircleImage1(calendar);
-                        break;
-                    case 1:
-                        robotoCalendarView.markCircleImage2(calendar);
-                        break;
-                    default:
-                        break;
-                }
+        markDayButton.setOnClickListener(view -> {
+            Calendar calendar = Calendar.getInstance();
+            Random random = new Random(System.currentTimeMillis());
+            int style = random.nextInt(2);
+            int daySelected = random.nextInt(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+            calendar.set(Calendar.DAY_OF_MONTH, daySelected);
+
+            switch (style) {
+                case 0:
+                    robotoCalendarView.markCircleImage1(calendar.getTime());
+                    break;
+                case 1:
+                    robotoCalendarView.markCircleImage2(calendar.getTime());
+                    break;
+                default:
+                    break;
             }
         });
+
+        clearSelectedDayButton.setOnClickListener(v -> robotoCalendarView.clearSelectedDay());
 
         // Set listener, in this case, the same activity
         robotoCalendarView.setRobotoCalendarListener(this);
